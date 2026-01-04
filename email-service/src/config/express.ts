aa7160458d6096dc1b37f6f439ce9802752405a6
Mohit <mohitmtnr@gmail.com>
2026-01-04 18:27:25 +0530

    removed cors from microservices

--- email-service/src/config/express.ts
+++ email-service/src/config/express.ts
@@ -1,5 +1,4 @@
 import express, { Express } from 'express';
-import cors from 'cors';
 import { urlencoded, json } from 'express';
 import env from './env';
 import pageNotFound from './404';
@@ -16,22 +15,6 @@ export function getExpressApp() {
 
 export function includeExpressMiddleware(app: Express) {
   //body parser
-  const allowedOrigins = env.ALLOWED_ORIGINS?.split(',').map((o) => o.trim());
-
-  app.use(
-    cors({
-      origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin) || origin.endsWith('.vercel.app')) {
-          return callback(null, true);
-        }
-        callback(new Error('Not allowed by CORS'));
-      },
-      credentials: true,
-    }),
-  );
-
-  app.options('/{*any}', cors());
   app.use(urlencoded({ extended: false }));
   app.use(json());
 }
