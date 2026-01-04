a90de8eb7cf384cffcfbce910ec17013199cd3b7
Mohit <mohitmtnr@gmail.com>
2026-01-04 18:28:47 +0530

    removed cors from microservices

--- auth-service/src/config/express.ts
+++ auth-service/src/config/express.ts
@@ -1,5 +1,4 @@
 import express, { Express } from 'express';
-import cors from 'cors';
 import { urlencoded, json } from 'express';
 import userRoute from '../routes/userRoute';
 import cookiParser from 'cookie-parser';
@@ -18,23 +17,6 @@ export function getExpressApp() {
 
 export function includeExpressMiddleware(app: Express) {
   //body parser
-
-  const allowedOrigins = env.ALLOWED_ORIGINS?.split(',').map((o) => o.trim());
-
-  app.use(
-    cors({
-      origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin) || origin.endsWith('.vercel.app')) {
-          return callback(null, true);
-        }
-        callback(new Error('Not allowed by CORS'));
-      },
-      credentials: true,
-    }),
-  );
-
-  app.options('/{*any}', cors());
   app.use(cookiParser());
   app.use(urlencoded({ extended: false }));
   app.use(json());
--- chat-service/src/config/express.ts
+++ chat-service/src/config/express.ts
@@ -1,5 +1,4 @@
 import express, { Express, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
 import { urlencoded, json } from 'express';
 import cookiParser from 'cookie-parser';
 import env from './env';
@@ -20,22 +19,6 @@ export function getExpressApp() {
 
 export function includeExpressMiddleware(app: Express) {
   //body parser
-  const allowedOrigins = env.ALLOWED_ORIGINS?.split(',').map((o) => o.trim());
-
-  app.use(
-    cors({
-      origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin) || origin.endsWith('.vercel.app')) {
-          return callback(null, true);
-        }
-        callback(new Error('Not allowed by CORS'));
-      },
-      credentials: true,
-    }),
-  );
-
-  app.options('/{*any}', cors());
   app.use(cookiParser());
   app.use(urlencoded({ extended: false }));
   app.use(json());
